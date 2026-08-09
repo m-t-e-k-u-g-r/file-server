eda62229aec611df9e905f55dad5bde6d74cafee
m-t-e-k-u-g-r <matteo.krueger.ch@gmail.com>
2026-08-09 16:09:01 +0200

    feat: add function to check the admin key

--- src/main/java/ch/mtekugr/fileserver/services/AuthService.java
+++ src/main/java/ch/mtekugr/fileserver/services/AuthService.java
@@ -1,12 +1,24 @@
 package ch.mtekugr.fileserver.services;
 
 import ch.mtekugr.fileserver.dtos.AccessKeyDto;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @Service
 public class AuthService {
+    private final Environment environment;
+
+    public AuthService(Environment environment) {
+        this.environment = environment;
+    }
+
+    public boolean checkAdminKey(String key) {
+        return Objects.equals(key, environment.getProperty("admin.key"));
+    }
+
     public AccessKeyDto validateKey(String key) {
         String[] parts = key.split("\\.", -1);
         if (parts.length != 2) throw new IllegalArgumentException("Access key has invalid structure");
