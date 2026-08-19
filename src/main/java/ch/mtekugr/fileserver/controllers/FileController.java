4386e4f2e093880a690788f5bfecc7f0a2edfb7f
m-t-e-k-u-g-r <matteo.krueger.ch@gmail.com>
2026-08-19 19:37:21 +0200

    fix: use fileService to delete uploaded file correctly

--- src/main/java/ch/mtekugr/fileserver/controllers/FileController.java
+++ src/main/java/ch/mtekugr/fileserver/controllers/FileController.java
@@ -1,12 +1,13 @@
 package ch.mtekugr.fileserver.controllers;
 
-import ch.mtekugr.fileserver.repositories.FileRepository;
 import ch.mtekugr.fileserver.services.FileService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.net.URI;
 import java.util.UUID;
@@ -15,11 +16,9 @@ import java.util.UUID;
 @RequestMapping("/files")
 public class FileController {
     private final FileService fileService;
-    private final FileRepository fileRepository;
 
-    public FileController(FileService fileService, FileRepository fileRepository) {
+    public FileController(FileService fileService) {
         this.fileService = fileService;
-        this.fileRepository = fileRepository;
     }
 
     @GetMapping("/{fileId}")
@@ -40,11 +39,15 @@ public class FileController {
     }
 
     @DeleteMapping("/{fileId}")
-    public ResponseEntity<Void> deleteFile(
-            @PathVariable UUID fileId
-    ) {
-        boolean removed = fileRepository.removeById(fileId);
-        if (removed) return ResponseEntity.noContent().build();
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<Void> deleteFile(@PathVariable UUID fileId) {
+        try {
+            fileService.deleteFile(fileId);
+            return ResponseEntity.noContent().build();
+        } catch (ResponseStatusException ex) {
+            if (ex.getStatusCode() == HttpStatus.NOT_FOUND) {
+                return ResponseEntity.notFound().build();
+            }
+            throw ex;
+        }
     }
 }
--- src/main/java/ch/mtekugr/fileserver/repositories/FileRepository.java
+++ src/main/java/ch/mtekugr/fileserver/repositories/FileRepository.java
@@ -7,6 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface FileRepository extends JpaRepository<File, UUID> {
-    @Transactional
-    boolean removeById(UUID id);
 }
